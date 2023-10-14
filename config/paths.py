059bc2a6442332f8e243ff5557d7407efe1c9aeb
Gilt0 <gil-arnaud.coche.2006@polytechnique.org>
2023-10-14 15:33:09 +0900

    refactoring + trained models

---
+++ config/paths.py
@@ -0,0 +1,5 @@
+DATA_PATH = '/Users/gil-arnaudcoche/Documents/ijoutaku/data/'
+UCSD_PATH = f'{DATA_PATH}UCSD_Anomaly_Dataset.v1p2/UCSDped1/'
+TRAIN_PATH = f'{UCSD_PATH}Train/'
+TEST_PATH = f'{UCSD_PATH}Test/'
+MODELS_PATH = f'{DATA_PATH}models/'
