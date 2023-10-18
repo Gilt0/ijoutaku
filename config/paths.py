a8797b457e545655c06ef1b0eb1d5a1f8e6590af
Gilt0 <gil-arnaud.coche.2006@polytechnique.org>
2023-10-18 14:48:40 +0900

    Flexible paths

--- config/paths.py
+++ config/paths.py
@@ -1,4 +1,7 @@
-DATA_PATH = '/Users/gil-arnaudcoche/Documents/ijoutaku/data/'
+import os
+
+HOME = f'{os.getcwd().replace("config", "")}'
+DATA_PATH = f'{HOME}data/'
 UCSD_PATH = f'{DATA_PATH}UCSD_Anomaly_Dataset.v1p2/UCSDped1/'
 TRAIN_PATH = f'{UCSD_PATH}Train/'
 TEST_PATH = f'{UCSD_PATH}Test/'
