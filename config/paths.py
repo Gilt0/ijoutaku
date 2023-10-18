bc644177b5085cd2267b60e41b3147d8632725bc
Gilt0 <gil-arnaud.coche.2006@polytechnique.org>
2023-10-18 15:00:50 +0900

    Updating path

--- config/paths.py
+++ config/paths.py
@@ -1,6 +1,6 @@
 import os
 
-HOME = f'{os.getcwd().replace("config", "")}'
+HOME = f'{os.getcwd()}/../'
 DATA_PATH = f'{HOME}data/'
 UCSD_PATH = f'{DATA_PATH}UCSD_Anomaly_Dataset.v1p2/UCSDped1/'
 TRAIN_PATH = f'{UCSD_PATH}Train/'
